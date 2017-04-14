1421089215f297bf7ea9ded6fa7dd69a7cdb91bc
Patrick <emmanuelpatrick25@gmail.com>
2017-04-14 14:37:59 +0100

    added favicon

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('views', path.join(__dirname, 'app_server/views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
